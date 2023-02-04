70502f64155ce63966cf010fef6c7a89f40a932d
Cosimo <cosimo.solana@gmail.com>
2023-02-04 11:01:10 -0500

    Collapse authenticated options into a dropdown menu

--- components/AuthenticationRow/AuthenticationRow.tsx
+++ components/AuthenticationRow/AuthenticationRow.tsx
@@ -1,8 +1,41 @@
 // import { useWallet } from "@solana/wallet-adapter-react";
 import { SolanaAuthButton } from "components/Auth/SolanaAuthButton";
 import { signOut, useSession } from "next-auth/react";
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import {
+  ArrowLeftOnRectangleIcon,
+  BellAlertIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { shortPubKey } from "utils";
+import { classNames, shortPubKey } from "utils";
+
+const navigationItems = [
+  {
+    name: "Mint an NFT",
+    description:
+      "A simple interface to create NFTs to your wallet, or send by email.",
+    href: "/mint",
+    icon: PlusCircleIcon,
+  },
+  {
+    name: "Account Settings",
+    description: "Mange your wallets, and notification subscriptions.",
+    href: "/account/wallets",
+    icon: BellAlertIcon,
+  },
+  {
+    name: "Logout",
+    description: "Sign out of your account.",
+    href: "#",
+    icon: ArrowLeftOnRectangleIcon,
+    onClick: () => {
+      signOut();
+    },
+  },
+];
 
 const AuthenticationRow: React.FC = () => {
   const { data: session, status } = useSession();
@@ -11,39 +44,82 @@ const AuthenticationRow: React.FC = () => {
   return (
     <>
       {session ? (
-        <div className="flex gap-2 text-slate-200">
-          <span>Signed in as:</span>
-          <span>
-            <Link href={`/wallet/${session.user?.id}`}>
-              <a>
-                {session.user?.account
-                  ? `@${session.user.account.username}`
-                  : shortPubKey(session.user?.id)}
-              </a>
-            </Link>
-          </span>
-          <span>|</span>
-          <span>
-            <Link href={`/mint`}>
-              <a>Mint an NFT</a>
-            </Link>
-          </span>
-          <span>|</span>
-          <span>
-            <Link href={`/account/wallets`}>
-              <a>Account Settings</a>
-            </Link>
-          </span>
-          <span>|</span>
-          <a
-            href="#"
-            onClick={() => {
-              signOut();
-            }}
-          >
-            Logout
-          </a>
-        </div>
+        <Popover className="relative">
+          {({ open }) => (
+            <>
+              <Popover.Button
+                className={classNames(
+                  open ? "text-indigo-500" : "text-indigo-400",
+                  "group inline-flex items-center rounded-md text-sm font-medium hover:text-indigo-300 py-3 pl-2"
+                )}
+              >
+                <span>
+                  welcome back{" "}
+                  {session.user?.account
+                    ? `@${session.user.account.username}`
+                    : shortPubKey(session.user?.id)}
+                </span>
+                <ChevronDownIcon
+                  className={classNames(
+                    open ? "text-indigo-500" : "text-indigo-400",
+                    "ml-1 h-5 w-5 group-hover:text-indigo-300"
+                  )}
+                  aria-hidden="true"
+                />
+              </Popover.Button>
+
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <Popover.Panel
+                  className={classNames(
+                    "absolute right-0 z-10 mt-3 w-screen max-w-md px-2 sm:px-0",
+                    navigationItems.length > 3 ? "lg:max-w-3xl" : ""
+                  )}
+                >
+                  <div className="overflow-hidden rounded-lg shadow-lg">
+                    <div
+                      className={classNames(
+                        "relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8",
+                        navigationItems.length > 3 ? "lg:grid-cols-2" : ""
+                      )}
+                    >
+                      {navigationItems.map((item) => (
+                        <Link href={item.href} key={item.name}>
+                          <a
+                            onClick={item.onClick}
+                            className="-m-3 flex items-start rounded-lg p-3 transition duration-150 ease-in-out hover:bg-gray-50"
+                          >
+                            <div className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
+                              <item.icon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            </div>
+                            <div className="ml-4">
+                              <p className="text-base font-medium text-gray-900 -mt-[2px]">
+                                {item.name}
+                              </p>
+                              <p className="mt-1 text-sm text-gray-500">
+                                {item.description}
+                              </p>
+                            </div>
+                          </a>
+                        </Link>
+                      ))}
+                    </div>
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </>
+          )}
+        </Popover>
       ) : // <>
       //   <button
       //     className="walletButton relative whitespace-nowrap"
