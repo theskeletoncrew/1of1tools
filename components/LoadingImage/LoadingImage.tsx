a11d2ff892da7356ab890b3725c517324c8c079b
Cosimo <cosimo.solana@gmail.com>
2023-02-05 17:57:14 -0500

    cleanup

--- components/LoadingImage/LoadingImage.tsx
+++ components/LoadingImage/LoadingImage.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useInView } from "react-intersection-observer";
-import JSXStyle from "styled-jsx/style";
 
 export type LoadingImgProps = Omit<
   React.DetailedHTMLProps<
