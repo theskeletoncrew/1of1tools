f86272b1ad113f3f9fc94a644327a1713fef3722
Cosimo <cosimo.solana@gmail.com>
2023-02-16 13:52:09 -0500

    Boutique: sort nfts by name

--- pages/boutique/[slug].tsx
+++ pages/boutique/[slug].tsx
@@ -6,7 +6,6 @@ import { useEffect, useState } from "react";
 import Header from "components/Header/Header";
 import ErrorMessage from "components/ErrorMessage/ErrorMessage";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { NFTS_PER_PAGE } from "utils/config";
 import LoadingIndicator from "components/LoadingIndicator/LoadingIndicator";
 import Layout from "components/Layout/Layout";
 import LoadingGrid from "components/LoadingGrid/LoadingGrid";
@@ -51,20 +50,14 @@ const CollectionPage: NextPage<Props> = ({ collection }) => {
 
   const [isLoading, setLoading] = useState(true);
   const [nftsMetadata, setNFTsMetadata] = useState<OneOfOneNFTMetadata[]>([]);
-  const [page, setPage] = useState(0);
-  const [hasMore, setHasMore] = useState(true);
   const [errorMessage, setErrorMessage] = useState<string>();
   const [listings, setListings] = useState<NFTListings[]>([]);
   const [filter, setFilter] = useState<NFTFilterType>(
     filterPref ? (filterPref as NFTFilterType) : NFTFilterType.ALL_ITEMS
   );
 
-  const getMoreNfts = async (
-    currentPage: number,
-    currentFilter: NFTFilterType
-  ) => {
+  const getMoreNfts = async (currentFilter: NFTFilterType) => {
     if (!collection.mintAddresses || collection.mintAddresses.length === 0) {
-      setHasMore(false);
       return;
     }
 
@@ -77,32 +70,16 @@ const CollectionPage: NextPage<Props> = ({ collection }) => {
             listings.find((l) => l.mint === m)
           );
 
-    const pageOfMintAddresses = mintAddresses.slice(
-      currentPage * NFTS_PER_PAGE,
-      (currentPage + 1) * NFTS_PER_PAGE
-    );
-
-    const nftsRes = await OneOfOneToolsClient.cachedNfts(pageOfMintAddresses);
+    const nftsRes = await OneOfOneToolsClient.cachedNfts(mintAddresses);
 
     if (nftsRes.isErr()) {
       toast.error("Failed to load more nfts: " + nftsRes.error.message);
     } else {
-      const newNFTMetadata =
-        currentPage === 0
-          ? nftsRes.value
-          : nftsRes.value.filter(
-              (n) => nftsMetadata.find((n2) => n2.mint === n.mint) === undefined
-            );
-
-      if (currentPage > 0) {
-        setNFTsMetadata((nftsMetadata) => [...nftsMetadata, ...newNFTMetadata]);
-      } else {
-        setNFTsMetadata(newNFTMetadata);
-      }
-      setHasMore(
-        (currentPage + 1) * NFTS_PER_PAGE < collection.mintAddresses.length
+      const newNFTMetadata = nftsRes.value.sort((nft1, nft2) =>
+        nft1.name.localeCompare(nft2.name)
       );
-      setPage(currentPage + 1);
+
+      setNFTsMetadata(newNFTMetadata);
     }
 
     setLoading(false);
@@ -127,7 +104,7 @@ const CollectionPage: NextPage<Props> = ({ collection }) => {
   };
 
   useEffect(() => {
-    getMoreNfts(0, filter);
+    getMoreNfts(filter);
   }, [filter, listings]);
 
   useEffect(() => {
@@ -207,19 +184,11 @@ const CollectionPage: NextPage<Props> = ({ collection }) => {
           {isLoading ? (
             <LoadingGrid className="mt-10 mx-1 gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 2xl:grid-cols-6" />
           ) : nftsMetadata.length > 0 ? (
-            <InfiniteScroll
-              dataLength={nftsMetadata.length}
-              next={() => getMoreNfts(page, filter)}
-              hasMore={hasMore}
-              loader={<LoadingIndicator />}
-              endMessage={""}
-            >
-              <CachedNFTGrid
-                nfts={nftsMetadata}
-                listings={listings}
-                isImported={true}
-              />
-            </InfiniteScroll>
+            <CachedNFTGrid
+              nfts={nftsMetadata}
+              listings={listings}
+              isImported={true}
+            />
           ) : (
             <ErrorMessage title={errorMessage ?? "No NFTs found"} />
           )}
